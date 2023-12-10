4d79f3e3444fe3e84b3c5c43379054bbba5097c9
AlexPetrov83 <zlordangel@yandex.ru>
2023-12-10 20:56:48 +0500

    222

---
+++ main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val hobbits = arrayOf("Frodo", "Sam", "Merry", "Pippin")
+    var x = 0
+
+    while (x < 5) {
+        println( "${hobbits[x]} is a good Hobbit name")
+        x += 1
+    }
+ }
