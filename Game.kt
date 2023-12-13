ebfbe1195ab4f07b0b36cd4dbacc2f21edd43df6
AlexPetrov83 <zlordangel@yandex.ru>
2023-12-14 00:43:30 +0500

    RockPaperScissors_3

--- Game.kt
+++ Game.kt
@@ -1,7 +1,14 @@
 fun main() {
 
- val options = arrayOf("Rock", "Paper", "Scissors")
- val gameChoice = getGameChoice(options)
- }
-fun getGameChoice (optionsParam: Array<String>) =
-  optionsParam[(Math.random() * optionsParam.size).toInt()]
+    val options = arrayOf("Rock", "Paper", "Scissors")
+    val gameChoice = getGameChoice(options)
+}
+
+fun getGameChoice(optionsParam: Array<String>) =
+    optionsParam[(Math.random() * optionsParam.size).toInt()]
+
+fun getUserChoice()
+    printLn("Please enter one of the following: Rock, Scissors, Paper.")
+
+
+        стр 105
